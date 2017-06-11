55c329bfdfae380e3314529798925dd5db17b890
hanatsuu <ryosuke.hanatsuka@jmty.jp>
2017-06-11 19:06:17 +0900

    form入力を可能にした

--- clowl_test.rb
+++ clowl_test.rb
@@ -1,23 +1,42 @@
 # coding: utf-8
 
+# coding: utf-8
+
 require 'open-uri'
 require 'open_uri_redirections'
 require 'nokogiri'
+require 'mechanize'
+require 'pry'
+
+agent = Mechanize.new
+page = agent.get("http://db.netkeiba.com/?pid=race_search_detail")
+form = page.forms[1]
+form.fields[4].value = 2000
+form.fields[6].value = 2017
+form.fields[7].value = 6
+form.checkboxes[10].click
 
 
-doc = Nokogiri::HTML(open('http://race.netkeiba.com/', :allow_redirections => :safe))
+p agent.submit(form)
 
-doc.css('a').each do |element|
-	# puts element[:href]
-	# puts element[:title]
-
+page.xpath("//label").each do |element|
+  puts element.text
 end
-
-doc.xpath('//td').each do |e|
-	# puts e.text
-end
+puts page.forms
 
 
-doc.xpath('//a').each do |e|
-	puts e.text
-end
+link = page.links
+
+
+
+# pid:race_list
+# word:
+# start_year:2000
+# start_mon:none
+# end_year:2017
+# end_mon:6
+# jyo[]:08
+# kyori_min:
+# kyori_max:
+# sort:date
+# list:100
