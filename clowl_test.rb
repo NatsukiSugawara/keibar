1272f0bf2038c99e58da78b2517fcc86b9594f0f
natsuki.sugawara <natsuki.sugawara@jmpc-059.local>
2017-06-11 12:17:25 +0900

    レース名取得

--- clowl_test.rb
+++ clowl_test.rb
@@ -8,11 +8,16 @@ require 'nokogiri'
 doc = Nokogiri::HTML(open('http://race.netkeiba.com/', :allow_redirections => :safe))
 
 doc.css('a').each do |element|
-	puts element[:href]
-	puts element[:title]
+	# puts element[:href]
+	# puts element[:title]
 
 end
 
 doc.xpath('//td').each do |e|
+	# puts e.text
+end
+
+
+doc.xpath('//a').each do |e|
 	puts e.text
 end
