9bed971e59335a8daf15b20672f6bd1de26e827d
natsuki.sugawara <natsuki.sugawara@jmpc-059.local>
2017-06-11 12:06:22 +0900

    first

---
+++ clowl_test.rb
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+require 'open-uri'
+require 'open_uri_redirections'
+require 'nokogiri'
+
+
+doc = Nokogiri::HTML(open('http://race.netkeiba.com/', :allow_redirections => :safe))
+
+doc.css('a').each do |element|
+	puts element[:href]
+	puts element[:title]
+
+end
+
+doc.xpath('//td').each do |e|
+	puts e.text
+end
