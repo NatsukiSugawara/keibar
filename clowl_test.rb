a2acb50023b0231854d51459b16d3f22af895024
hanatsuu <ryosuke.hanatsuka@jmty.jp>
2017-06-12 00:54:48 +0900

    各レースの結果取得

--- clowl_test.rb
+++ clowl_test.rb
@@ -1,42 +1,36 @@
-# coding: utf-8
-
-# coding: utf-8
-
 require 'open-uri'
 require 'open_uri_redirections'
 require 'nokogiri'
 require 'mechanize'
 require 'pry'
 
-agent = Mechanize.new
-page = agent.get("http://db.netkeiba.com/?pid=race_search_detail")
-form = page.forms[1]
-form.fields[4].value = 2000
-form.fields[6].value = 2017
-form.fields[7].value = 6
-form.checkboxes[10].click
+class Crawler
+  def self.search_page
+    agent = Mechanize.new
+    page = agent.get('http://db.netkeiba.com/?pid=race_search_detail')
+    form = page.forms[1]
+    form.fields[4].value = 2000 # start_year
+    form.fields[5].value = 1    # start_mon
+    form.fields[6].value = 2017 # end_year
+    form.fields[7].value = 6    # end_mon
+    form.checkboxes[10].click   # 競馬場
 
+    agent.submit(form)
+  end
 
-p agent.submit(form)
+  def self.crawl(page)
+    hrefs = page.search("//div[@id='page']/div[@id='contents_liquid']/table/tr/td/a")
+    links = []
 
-page.xpath("//label").each do |element|
-  puts element.text
+    hrefs.each do |href|
+      link.push(page.link_with(href: href.attributes['href'].value))
+    end
+
+    links.each do |link|
+      page = link.click
+    end
+  end
 end
-puts page.forms
 
-
-link = page.links
-
-
-
-# pid:race_list
-# word:
-# start_year:2000
-# start_mon:none
-# end_year:2017
-# end_mon:6
-# jyo[]:08
-# kyori_min:
-# kyori_max:
-# sort:date
-# list:100
+page = Crawler.search_page
+Crawler.crawl(page)
